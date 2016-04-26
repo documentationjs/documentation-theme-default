36437b73eb0825e9cbf9df1b69617abae9827e59
Tom MacWright <tom@macwright.org>
2016-04-26 17:58:04 -0400

    Port templates to lodash

--- index.js
+++ index.js
@@ -4,19 +4,14 @@ var fs = require('fs'),
   path = require('path'),
   File = require('vinyl'),
   vfs = require('vinyl-fs'),
+  _ = require('lodash'),
   concat = require('concat-stream'),
-  Handlebars = require('handlebars'),
   formatMarkdown = require('./lib/format_markdown'),
   formatParameters = require('./lib/format_parameters');
 
 module.exports = function (comments, options, callback) {
-  var pageTemplate = Handlebars.compile(fs.readFileSync(path.join(__dirname, 'index.hbs'), 'utf8'));
 
-  Handlebars.registerPartial('section',
-    Handlebars.compile(fs.readFileSync(path.join(__dirname, 'section.hbs'), 'utf8'), {
-      preventIndent: true
-    })
-  );
+  var highlight = require('./lib/highlight')(options.hljs || {});
 
   var paths = comments.map(function (comment) {
     return comment.path.join('.');
@@ -24,28 +19,42 @@ module.exports = function (comments, options, callback) {
     return path;
   });
 
-  Handlebars.registerHelper('permalink', function () {
-    return this.path.join('.');
+  var pageTemplate = _.template(fs.readFileSync(path.join(__dirname, 'index.hbs'), 'utf8'), {
+    imports: {
+      section: _.template(fs.readFileSync(path.join(__dirname, 'section.hbs'), 'utf8'), {
+        imports: {
+          permalink: function(path) {
+            return path.join('.');
+          },
+          md: function (string) {
+            return formatMarkdown(string, paths);
+          },
+          formatType: function(section) {
+            return formatMarkdown.type(section.type, paths);
+          },
+          formatParameters: formatParameters
+        }
+      }),
+      permalink: function(path) {
+        return path.join('.');
+      },
+      highlight: function(str) {
+        return highlight(str);
+      }
+    }
   });
 
-  Handlebars.registerHelper('format_params', formatParameters);
 
-  Handlebars.registerHelper('md', function (string) {
-    return new Handlebars.SafeString(formatMarkdown(string, paths));
-  });
+  // Handlebars.registerHelper('format_params', formatParameters);
 
-  Handlebars.registerHelper('format_type', function (type) {
-    return new Handlebars.SafeString(formatMarkdown.type(type, paths));
-  });
 
-  Handlebars.registerHelper('autolink', function (text) {
-    return new Handlebars.SafeString(formatMarkdown.link(paths, text));
-  });
+  // Handlebars.registerHelper('format_type', function (type) {
+  //   return new Handlebars.SafeString(formatMarkdown.type(type, paths));
+  // });
 
-  var highlight = require('./lib/highlight')(options.hljs || {});
-  Handlebars.registerHelper('highlight', function (string) {
-    return new Handlebars.SafeString(highlight(string));
-  });
+  // Handlebars.registerHelper('autolink', function (text) {
+  //   return new Handlebars.SafeString(formatMarkdown.link(paths, text));
+  // });
 
   // push assets into the pipeline as well.
   vfs.src([__dirname + '/assets/**'], { base: __dirname })
@@ -58,4 +67,4 @@ module.exports = function (comments, options, callback) {
         }), 'utf8')
       })));
     }));
-};
+}
